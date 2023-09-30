9615b7732935994e452d4cf16d2fa8aa6ad0884d
BMSales <bruno.m.sales03@gmail.com>
2023-09-30 16:10:03 -0300

    Got the program working

--- Source.cpp
+++ Source.cpp
@@ -7,32 +7,36 @@ void genArray(int** array, int n) {
 	srand(time(0));
 	(*array) = (int*)malloc(n * sizeof(int));
 	for (int i = 0; i < n; i++) {
-		(*array)[i] = rand() % 1000;
+		(*array)[i] = rand() % 100;
 	}
 }
+
 void showArray(int* array, int n) {
 	for (int i = 0; i < n; i++) {
-		printf("%3d ", array[i]);
+		printf("%2d ", array[i]);
 	}
 	printf("\n");
 }
 
-void firstQuick(int* array, int n) {
-	int index = 0;
-	int pivot = n - 1;
+int Quick(int* array, int start, int pivot, int greater) {
+	if (pivot <= start) {
+		return 0;
+	}
 	int change;
-
-	for (int i = 0; i < pivot; i++) {
+	for (int i = start; i < pivot; i++) {
 		if (array[pivot] >= array[i]) {
 			change = array[i];
-			array[i] = array[index];
-			array[index] = change;
-			index++;
+			array[i] = array[greater];
+			array[greater] = change;
+			greater++;
 		}
 	}
 	change = array[pivot];
-	array[pivot] = array[index];
-	array[index] = change;
+	array[pivot] = array[greater];
+	array[greater] = change;
+
+	Quick(array, start, greater - 1, start);
+	Quick(array, greater + 1, pivot, greater + 1);
 }
 
 int main() {
@@ -41,7 +45,8 @@ int main() {
 	scanf_s("%d", &n);
 	genArray(&array, n);
 	showArray(array, n);
-	firstQuick(array, n);
+	Quick(array, 0, n - 1, 0);
 	showArray(array, n);
+	free(array);
 	return 0;
 }
