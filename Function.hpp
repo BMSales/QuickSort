e12c05b07fd08d160a91b83a8df97487c490ffc4
Bruno Sales <bruno.m.sales03@gmail.com>
2024-09-19 08:27:07 -0300

    the old files didn't go away

--- Function.hpp
+++
@@ -1,6 +0,0 @@
-#pragma once
-void Array_Populate(int**, int);
-void Array_Show(int*, int);
-int Quicksort_First(int*, int, int, int);
-int Quicksort_Last(int*, int, int, int);
-int Quicksort_Random(int*, int, int, int);
